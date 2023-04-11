193e9b0d317f42a57e9a5fccdc0489b6d06c2192
Claude Stephane Manace Kouame <47413448+kowalski2019@users.noreply.github.com>
2023-04-11 14:33:06 +0200

    improve the digit_to_char function

--- lib/string_builder.c
+++ lib/string_builder.c
@@ -86,42 +86,7 @@ char char_at(uint16_t index)
 
 char digit_to_char(uint32_t n)
 {
-	switch (n)
-	{
-	case 0:
-		return '0';
-		break;
-	case 1:
-		return '1';
-		break;
-	case 2:
-		return '2';
-		break;
-	case 3:
-		return '3';
-		break;
-	case 4:
-		return '4';
-		break;
-	case 5:
-		return '5';
-		break;
-	case 6:
-		return '6';
-		break;
-	case 7:
-		return '7';
-		break;
-	case 8:
-		return '8';
-		break;
-	case 9:
-		return '9';
-		break;
-	default:
-		return '\0';
-		break;
-	}
+	return (char)('0' + n);
 }
 
 void int_to_string(int32_t n)
